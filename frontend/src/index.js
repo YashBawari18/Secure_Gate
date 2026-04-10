4798b164046147144fd8a6b01bddc52a08af1091
Yash Sunder Bawari <yashsunderbawari@Yashs-MacBook-Air.local>
2026-04-11 00:31:08 +0530

    feat: dark mode, multilingual support (EN/HI/MR/GU), theme + language toggle in topbar

--- frontend/src/index.js
+++ frontend/src/index.js
@@ -1,10 +1,14 @@
 import React from 'react';
 import ReactDOM from 'react-dom/client';
 import App from './App';
+import './i18n'; // Initialise i18next
+import { ThemeProvider } from './contexts/ThemeContext';
 
 const root = ReactDOM.createRoot(document.getElementById('root'));
 root.render(
   <React.StrictMode>
-    <App />
+    <ThemeProvider>
+      <App />
+    </ThemeProvider>
   </React.StrictMode>
 );
